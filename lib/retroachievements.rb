d35b9a23abe0b197b0963b8d8f9375b516c703bb
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-16 18:37:36 -0300

    chore: update main module imports and bump version

--- lib/retroachievements.rb
+++ lib/retroachievements.rb
@@ -3,14 +3,20 @@
 require 'net/http'
 require 'json'
 
-require_relative 'retro_cheevos/client'
-require_relative 'retro_cheevos/resources/base'
-require_relative 'retro_cheevos/resources/game'
-require_relative 'retro_cheevos/resources/user'
-require_relative 'retro_cheevos/utils/errors'
+require_relative 'retroachievements/client'
+require_relative 'retroachievements/resources/achievement'
+require_relative 'retroachievements/resources/base'
+require_relative 'retroachievements/resources/comment'
+require_relative 'retroachievements/resources/feed'
+require_relative 'retroachievements/resources/game'
+require_relative 'retroachievements/resources/leaderboard'
+require_relative 'retroachievements/resources/system'
+require_relative 'retroachievements/resources/ticket'
+require_relative 'retroachievements/resources/user'
+require_relative 'retroachievements/utils/errors'
 
 #
 # Main module for the wrapper
 module RA
-  VERSION = '0.0.1'
+  VERSION = '0.1.0'
 end
